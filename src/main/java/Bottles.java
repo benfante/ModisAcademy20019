592e3121392bd52664f8a7bd9b6a00d984fa206f
Lucio Benfante <lucio@benfante.com>
2019-12-12 16:35:36 +0100

    Insuleated bottles strings

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -8,25 +8,25 @@ class Bottles {
     public static void writeLyrics(PrintStream out) {
         for (int beers = 99; beers > -1; beers--) {
             if (beers == 0) {
-                out.print(beers + " bottles of beer on the wall,");
-                out.println(beers + " bottles of beer,");
+                out.print(beers + " bottles" + " of beer on the wall,");
+                out.println(beers + " bottles" + " of beer,");
                 out.print("Go to the store, buy some more,");
-                out.println("99 bottles of beer on the wall.\r\n");
+                out.println("99 bottles" + " of beer on the wall.\r\n");
             } else if (beers == 1) {
-                out.print(beers + " bottle of beer on the wall,");
-                out.println(beers + " bottle of beer,");
+                out.print(beers + " bottle" + " of beer on the wall,");
+                out.println(beers + " bottle" + " of beer,");
                 out.print("Take one down, pass it around,");
-                out.println(beers - 1 + " bottles of beer on the wall.\r\n");
+                out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
             } else if (beers == 2) {
-                out.print(beers + " bottles of beer on the wall,");
-                out.println(beers + " bottles of beer,");
+                out.print(beers + " bottles" + " of beer on the wall,");
+                out.println(beers + " bottles" + " of beer,");
                 out.print("Take one down, pass it around,");
-                out.println(beers - 1 + " bottle of beer on the wall.\r\n");
+                out.println(beers - 1 + " bottle" + " of beer on the wall.\r\n");
             } else {
-                out.print(beers + " bottles of beer on the wall,");
-                out.println(beers + " bottles of beer,");
+                out.print(beers + " bottles" + " of beer on the wall,");
+                out.println(beers + " bottles" + " of beer,");
                 out.print("Take one down, pass it around,");
-                out.println(beers - 1 + " bottles of beer on the wall.\r\n");
+                out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
             }
         }
     }
