45ef330660de0fc8373f3c49f9a2a406fbf4ec5f
Lucio Benfante <lucio@benfante.com>
2019-12-12 15:45:54 +0100

    Removed s variable for 0 scenario

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -9,8 +9,8 @@ class Bottles {
         String s = "s";
         for (int beers = 99; beers > -1; beers--) {
             if (beers == 0) {
-                out.print(beers + " bottle" + s + " of beer on the wall,");
-                out.println(beers + " bottle" + s + " of beer,");
+                out.print(beers + " bottles of beer on the wall,");
+                out.println(beers + " bottles of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
                 return;
